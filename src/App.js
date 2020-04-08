43666646c68da2a0d175762f827b8777982523fc
Nha Phan <nhaphan0073@gmail.com>
2020-04-08 17:42:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const render_row = (first_name, last_name, point, number) => {
+  let row = [];
+  row.push(<td>{first_name}</td>);
+  row.push(<td>{last_name}</td>);
+  for (let i = 0; i < number; i++) {
+    row.push(<td>{point}</td>);
+  }
+  return <tr>{row}</tr>;
+};
+
 function App() {
+  const num = 50;
+  let points_th = [];
+
+  for(let i = 0; i < num; i++ ) {
+    points_th.push(<th>Points</th>);
+  }
+
+  let row_1 = render_row('Jill', 'Smith', 50, num);
+  let row_2 = render_row('Eva', 'Jackson', 94, num);
+  let row_3 = render_row('Adam', 'Johnson', 67, num);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div className='container'>
+          <table>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              {points_th}
+            </tr>
+              {row_1}
+              {row_2}
+              {row_3}
+          </table>
+        </div>
+      </div>
   );
 }
 
